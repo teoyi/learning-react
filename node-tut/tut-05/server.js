08498d882fc1f49d25a35bc999cf0de81aae722b
YiPeng Teo <yipengteo@yahoo.com.sg>
2022-02-04 16:37:51 +0800

    completed chapter 5

---
+++ node-tut/tut-05/server.js
@@ -0,0 +1,121 @@
+// common imports needed 
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+
+// custom imports 
+const logEvents = require('./logEvents');
+const EventEmitter = require('events');
+const { eachQuarterOfInterval } = require('date-fns');
+class Emitter extends EventEmitter {};
+
+// initialize object 
+const myEmitter = new Emitter();
+myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName));
+// port establishment 
+const PORT = process.env.PORT || 3500; 
+
+const serveFile = async (filePath, contentType, response) => {
+    try{
+        const rawData = await fsPromises.readFile(
+            filePath, 
+            !contentType.includes('image')? 'utf8' : '');
+        const data = contentType === 'application/json'
+            ? JSON.parse(rawData) : rawData;
+        response.writeHead(
+            filePath.includes('404.html') ? 404: 200, 
+            {'Content-Type': contentType});
+        response.end(
+            contentType === 'application/json' ? JSON.stringify(data) : data
+        );
+    } catch (err) {
+        console.log(err);
+        myEmitter.emit('log', `${err.name}: ${err.message}`, 'errLog.txt');
+        response.statusCode = 500; 
+        response.end(); 
+    }
+}
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+    myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt');
+    const extension = path.extname(req.url); 
+
+    let contentType; 
+
+    switch (extension) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.jpg':
+            contentType = 'image/jpeg';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.txt':
+            contentType = 'text/plain';
+            break;
+        default:
+            contentType = 'text/html';
+    };
+
+    let filePath =
+    contentType === 'text/html' && req.url === '/'
+        ? path.join(__dirname, 'views', 'index.html')
+        : contentType === 'text/html' && req.url.slice(-1) === '/'
+            ? path.join(__dirname, 'views', req.url, 'index.html')
+            : contentType === 'text/html'
+                ? path.join(__dirname, 'views', req.url)
+                : path.join(__dirname, req.url);
+
+    // makes .html extension not required in the browser 
+    if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
+
+    const fileExists = fs.existsSync(filePath);
+    
+    if (fileExists) {
+        // serve file 
+        serveFile(filePath, contentType, res);
+    } else { 
+        switch(path.parse(filePath).base){
+            case 'old-page.html':
+                res.writeHead(301, {'Location': '/new-page.html'});
+                res.end();
+                break;
+            case 'www-page.html':
+                res.writeHead(301, {'Location': '/'});
+                res.end();
+                break;
+            default:
+                // serve 404 
+                serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
+        }
+    }
+});
+
+// listen will always be at the bottom of the script 
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+
+
+
+
+
+
+
+
+
+// // add listener for the log event 
+// myEmitter.on('log', (msg) => logEvents(msg));
+// // Emit event 
+// myEmitter.emit('log', 'Log event emitted!');
