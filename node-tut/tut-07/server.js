0e65185a17b246089e229c74f8b47961fcc20595
YiPeng Teo <yipengteo@yahoo.com.sg>
2022-02-05 17:04:08 +0800

    Completed chapter 7

--- node-tut/tut-07/server.js
+++ node-tut/tut-07/server.js
@@ -1,7 +1,39 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
+
+// custom middleware logger
+app.use(logger);
+
+// Cross origin resource sharing
+// corsoptions is needed to determine if a certain domain is enabled to have access to the routes
+const whitelist = ['https://www.yoursite.com', 'http://126.0.0.1:5500', 'http://localhost:3500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true) // if no errorrs, cakll back will have null 
+        } else {
+            callback(new Error('Note allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
+// built-in middleware to handle urlencoded data 
+// in other words, form data: 
+// 'content-type': application/x-www-form-urlencoded' 
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json 
+app.use(express.json());
+
+// serve static files
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('^/$|/index(.html)?', (req, res) => {
     //res.sendFile('./views/index.html', { root: __dirname });
@@ -43,8 +75,18 @@ const three = (req, res) => {
 
 app.get('/chain(.html)?', [one, two, three]);
 
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-})
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')){
+        res.sendFile(path.join(__dirname, 'views', '404.html'));    
+    } else if (req.accepts('json')){
+        res.json({ error: "404 not found"});    
+    } else {
+        res.type('txt').send("404 not found");
+    }
+});
+
+//error handling
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
